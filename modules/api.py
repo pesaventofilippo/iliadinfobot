1a12d0055f6269bb07d63498d08b41d28bfd90ae
Filippo Pesavento <pesaventofilippo@gmail.com>
2021-09-06 12:03:50 +0200

    Updated new APIs

--- modules/api.py
+++ modules/api.py
@@ -1,5 +1,6 @@
 from requests import session as reqSession
 from lxml import html
+import re
 
 
 class AuthenticationFailedError(Exception):
@@ -10,34 +11,56 @@ class AuthenticationFailedError(Exception):
 class IliadApi:
     loginUrl = "https://www.iliad.it/account/"
 
-    xpaths = {
-        "loginError":    "//div[@class='flash flash-error']/text()",
-        "credito":       "//*[@id='page-container']/div/div[2]/div[2]/div[9]/div[2]",
-        "rinnovo":       "//*[@id='page-container']/div/div[2]/div[2]/div[4]",
-        "nome":          "//*[@id='page-container']/div/nav/div/div/div[2]/div[1]",
-        "id":            "//*[@id='page-container']/div/nav/div/div/div[2]/div[2]",
-        "numero":        "//*[@id='page-container']/div/nav/div/div/div[2]/div[3]",
+    def xpaths(self, name: str, estero: bool=False):
+        paths = {
+            "_base":  "//*[@id='page-container']/div/",
+            "notext": ["gigaTot"],
 
-        "chiamateCount": "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[1]/div[1]/div/div[1]/span[1]",
-        "chiamateCosto": "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[1]/div[1]/div/div[1]/span[2]",
-        "smsCount":      "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[1]/div[2]/div/div[1]/span[1]",
-        "smsCosto":      "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[1]/div[2]/div/div[1]/span[2]",
-        "gigaCount":     "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[2]/div[1]/div/div[1]/span[1]",
-        "gigaTot":       "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[2]/div[1]/div/div[1]/text()[1]",
-        "gigaCosto":     "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[2]/div[1]/div/div[1]/span[2]",
-        "mmsCount":      "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[2]/div[2]/div/div[1]/span[1]/text()",
-        "mmsCosto":      "//*[@id='page-container']/div/div[2]/div[2]/div[5]/div[2]/div[2]/div/div[1]/span[2]",
+            "special": {
+                "loginError": "//div[@class='flash flash-error']"
+            },
 
-        "ext-chiamateCount": "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[1]/div[1]/div/div[1]/span[1]",
-        "ext-chiamateCosto": "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[1]/div[1]/div/div[1]/span[2]",
-        "ext-smsCount":      "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[1]/div[2]/div/div[1]/span[1]",
-        "ext-smsCosto":      "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[1]/div[2]/div/div[1]/span[2]",
-        "ext-gigaCount":     "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[2]/div[1]/div/div[1]/span[1]",
-        "ext-gigaTot":       "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[2]/div[1]/div/div[1]/text()[1]",
-        "ext-gigaCosto":     "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[2]/div[1]/div/div[1]/span[2]",
-        "ext-mmsCount":      "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[2]/div[2]/div/div[1]/span[1]/text()",
-        "ext-mmsCosto":      "//*[@id='page-container']/div/div[2]/div[2]/div[6]/div[2]/div[2]/div/div[1]/span[2]"
-    }
+            "basic": {
+                "_base":   "",
+                "credito": "div[2]/div[2]/div[9]/div[2]",
+                "rinnovo": "div[2]/div[2]/div[4]",
+                "nome":    "nav/div/div/div[2]/div[1]",
+                "id":      "nav/div/div/div[2]/div[2]",
+                "numero":  "nav/div/div/div[2]/div[3]"
+            },
+
+            "info": {
+                "_base":      "div[2]/div[2]/",
+                "_intSuffix": "div[5]/",
+                "_extSuffix": "div[6]/",
+
+                "chiamateCount": "div[1]/div[1]/div/div[1]/span[1]",
+                "chiamateCosto": "div[1]/div[1]/div/div[1]/span[2]",
+                "smsCount":      "div[1]/div[2]/div/div[1]/span[1]",
+                "smsCosto":      "div[1]/div[2]/div/div[1]/span[2]",
+                "gigaCount":     "div[2]/div[1]/div/div[1]/span[1]",
+                "gigaTot":       "div[2]/div[1]/div/div[1]/text()[1]",
+                "gigaCosto":     "div[2]/div[1]/div/div[1]/span[2]",
+                "mmsCount":      "div[2]/div[2]/div/div[1]/span[1]",
+                "mmsCosto":      "div[2]/div[2]/div/div[1]/span[2]"
+            }
+        }
+
+        if name in paths["special"].keys():
+            selected = paths["special"]["name"]
+
+        else:
+            cat = "info" if name in paths["info"].keys() else "basic"
+            selected = paths["_base"] + paths[cat]["_base"]
+            if cat == "info":
+                suffix = "_intSuffix" if not estero else "_extSuffix"
+                selected += paths["info"][suffix]
+            selected += paths[cat][name]
+
+        if name not in paths["notext"]:
+            selected += "/text()"
+
+        return selected
 
 
     def __init__(self, username: str, password: str):
@@ -57,7 +80,7 @@ class IliadApi:
             resp = sess.post(self.loginUrl, loginInfo)
         tree = html.fromstring(resp.content)
 
-        error = tree.xpath(self.xpaths['loginError'])
+        error = tree.xpath(self.xpaths("loginError"))
         if error:
             raise AuthenticationFailedError
 
@@ -66,13 +89,13 @@ class IliadApi:
 
     # float: euros
     def credito(self):
-        return float(self.page.xpath(self.xpaths['credito'])[0].replace("€", ""))
+        return float(self.page.xpath(self.xpaths("credito"))[0].replace("€", ""))
 
 
     # datetime: contract renewal
     def dataRinnovo(self):
         from datetime import datetime
-        raw = self.page.xpath(self.xpaths['rinnovo'])[0]\
+        raw = self.page.xpath(self.xpaths("rinnovo"))[0]\
             .replace("\n         La tua offerta iliad si rinnoverà alle ", "")\
             .replace("\n      ", "")
         return datetime.strptime(raw, "%H:%M del %d/%m/%Y")
@@ -80,79 +103,69 @@ class IliadApi:
 
     # string: holder name
     def nome(self):
-        return self.page.xpath(self.xpaths['nome'])[0]
+        return self.page.xpath(self.xpaths("nome"))[0]
 
 
     # int: account id
     def id(self):
-        return int(self.page.xpath(self.xpaths['id'])[0].replace("ID utente: ", ""))
+        return int(self.page.xpath(self.xpaths("id"))[0].replace("ID utente: ", ""))
 
 
     # string: phone number (with spaces and no country code)
     def numero(self):
-        return self.page.xpath(self.xpaths['numero'])[0].replace("Numero: ", "")
+        return self.page.xpath(self.xpaths("numero"))[0].replace("Numero: ", "")
 
 
     # string: duration
     def totChiamate(self, estero: bool=False):
-        xpath = "chiamateCount" if not estero else "ext-chiamateCount"
-        raw = self.page.xpath(self.xpaths[xpath])[0].lower()
-        return raw
+        return self.page.xpath(self.xpaths("chiamateCount", estero))[0].lower()
 
 
     # float: euros
     def costoChiamate(self, estero: bool=False):
-        xpath = "chiamateCosto" if not estero else "ext-chiamateCosto"
-        return float(self.page.xpath(self.xpaths[xpath])[0].replace("€", ""))
+        return float(self.page.xpath(self.xpaths("chiamateCosto", estero))[0].replace("€", ""))
 
 
     # int: sms count
     def totSms(self, estero: bool=False):
-        xpath = "smsCount" if not estero else "ext-smsCount"
-        return int(self.page.xpath(self.xpaths[xpath])[0].replace(" SMS", ""))
+        return int(self.page.xpath(self.xpaths("smsCount", estero))[0].replace(" SMS", ""))
 
 
     # float: euros
     def costoSms(self, estero: bool=False):
-        xpath = "smsCosto" if not estero else "ext-smsCosto"
-        return float(self.page.xpath(self.xpaths[xpath])[0].replace("€", ""))
+        return float(self.page.xpath(self.xpaths("smsCosto", estero))[0].replace("€", ""))
 
 
     # dict: count(float) and unit(str)
     def totGiga(self, estero: bool=False):
-        xpath = "gigaCount" if not estero else "ext-gigaCount"
-        raw = self.page.xpath(self.xpaths[xpath])[0].upper()
-        if raw == "0B":
-            count = 0
-            unit = "B"
-        else:
-            unit = raw[-2:]
-            count = float(raw.replace(unit, "").replace(",", "."))
-        return {"count": count, "unit": unit}
+        raw = self.page.xpath(self.xpaths("gigaCount", estero))[0].upper()
+        split = re.split('(\d+)', raw)
+        return {
+            "count": int("".join(split[:-1]).replace(",", ".")),
+            "unit":  str(split[-1])
+        }
 
 
     # float: euros
     def costoGiga(self, estero: bool=False):
-        xpath = "gigaCosto" if not estero else "ext-gigaCosto"
-        return float(self.page.xpath(self.xpaths[xpath])[0].replace("€", ""))
+        return float(self.page.xpath(self.xpaths("gigaCosto", estero))[0].replace("€", ""))
 
 
     # dict: count(int) and unit(str)
     def pianoGiga(self, estero: bool=False):
-        xpath = "gigaTot" if not estero else "ext-gigaTot"
-        raw = self.page.xpath(self.xpaths[xpath])[0].replace(" / ", "").upper()
-        unit = raw[-2:]
-        count = int(raw.replace(unit, ""))
-        return {"count": count, "unit": unit}
+        raw = self.page.xpath(self.xpaths("gigaTot", estero))[0].replace(" / ", "").upper()
+        split = re.split('(\d+)', raw)
+        return {
+            "count": int("".join(split[:-1]).replace(",", ".")),
+            "unit": str(split[-1])
+        }
 
 
     # int: count
     def totMms(self, estero: bool=False):
-        xpath = "mmsCount" if not estero else "ext-mmsCount"
-        return int(self.page.xpath(self.xpaths[xpath])[0].replace(" MMS", ""))
+        return int(self.page.xpath(self.xpaths("mmsCount", estero))[0].replace(" MMS", ""))
 
 
     # float: euros
     def costoMms(self, estero: bool=False):
-        xpath = "mmsCosto" if not estero else "ext-mmsCosto"
-        return float(self.page.xpath(self.xpaths[xpath])[0].replace("€", ""))
+        return float(self.page.xpath(self.xpaths("mmsCosto", estero))[0].replace("€", ""))
